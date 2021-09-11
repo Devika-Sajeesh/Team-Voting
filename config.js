881fb3987603a81d11d824c2a4a346e6a0dee7a8
Devika Sajeesh <devikasajeesh26@gmail.com>
2021-09-11 11:08:51 +0530

    modified config.js

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: 'AIzaSyBTBvsstYUZXxw0Qyv1ZDVVauCS2b1uhJ4',
+  authDomain: 'voting-app-a89ec.firebaseapp.com',
+  databaseURL: 'https://voting-app-a89ec-default-rtdb.firebaseio.com/',
+  projectId: 'voting-app-a89ec',
+  storageBucket: 'voting-app-a89ec.appspot.com',
+  messagingSenderId: '461519218278',
+  appId: '1:461519218278:web:1b1f380e1594f5af32afa7',
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default firebase.database();
